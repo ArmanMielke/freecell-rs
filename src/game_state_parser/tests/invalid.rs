c63792c3b8bdf791a9a9e5eca1c90a09d267b895
Arman Mielke <arman.mielke@yahoo.de>
2018-11-06 19:36:04 +0100

    Add tests for invalid game state files

---
+++ src/game_state_parser/tests/invalid.rs
@@ -0,0 +1,49 @@
+use super::super::parse_file;
+use super::super::error_messages::ERR_TOO_MANY_CASCADES;
+
+
+#[test]
+fn test_easy_10_instead_of_t() {
+    let actual = parse_file("example-inputs/invalid/easy-10-instead-of-T.txt");
+    let expected = Err(err_card_code_not_length_2!("10S"));
+    assert_eq!(actual, expected);
+}
+
+//#[test]
+fn test_easy_duplicate_card() {
+    // TODO
+    let actual = parse_file("example-inputs/invalid/easy-duplicate-card.txt");
+    let expected = Err("TODO".to_string());
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_easy_extra_cascade() {
+    let actual = parse_file("example-inputs/invalid/easy-extra-cascade.txt");
+    let expected = Err(String::from(ERR_TOO_MANY_CASCADES));
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_easy_invalid_card_value() {
+    let actual = parse_file("example-inputs/invalid/easy-invalid-card-value.txt");
+    let expected = Err(err_could_not_parse_card_value!('X'));
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_easy_invalid_suit() {
+    let actual = parse_file("example-inputs/invalid/easy-invalid-suit.txt");
+    let expected = Err(err_could_not_parse_suit!('X'));
+    assert_eq!(actual, expected);
+}
+
+//#[test]
+fn test_easy_missing_card() {
+    // TODO
+}
+
+//#[test]
+fn test_easy_missing_card_2() {
+    // TODO
+}
--- src/game_state_parser/tests/mod.rs
+++ src/game_state_parser/tests/mod.rs
@@ -1,1 +1,2 @@
 mod test_hard;
+mod invalid;
