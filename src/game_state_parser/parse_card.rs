5404b9acb3c4b910aa439c44aed29ccea2e98632
Arman Mielke <arman.mielke@yahoo.de>
2018-11-06 19:40:35 +0100

    extract functions from parse_card(...)

--- src/game_state_parser/parse_card.rs
+++ src/game_state_parser/parse_card.rs
@@ -30,33 +30,40 @@ pub fn parse_card(card_code: &str) -> Result<Card, String> {
     let value_char = card_chars.next().unwrap();
     let suit_char = card_chars.next().unwrap();
 
-    let value = match value_char {
-        'A' => Rank(ACE),
-        '2' => Rank(2),
-        '3' => Rank(3),
-        '4' => Rank(4),
-        '5' => Rank(5),
-        '6' => Rank(6),
-        '7' => Rank(7),
-        '8' => Rank(8),
-        '9' => Rank(9),
-        'T' => Rank(10),
-        'J' => Rank(JACK),
-        'Q' => Rank(QUEEN),
-        'K' => Rank(KING),
+    Ok(Card {
+        suit: parse_suit(suit_char)?,
+        value: parse_value(value_char)?,
+    })
+}
+
+fn parse_value(value_char: char) -> Result<Rank, String> {
+    match value_char {
+        'A' => Ok(Rank(ACE)),
+        '2' => Ok(Rank(2)),
+        '3' => Ok(Rank(3)),
+        '4' => Ok(Rank(4)),
+        '5' => Ok(Rank(5)),
+        '6' => Ok(Rank(6)),
+        '7' => Ok(Rank(7)),
+        '8' => Ok(Rank(8)),
+        '9' => Ok(Rank(9)),
+        'T' => Ok(Rank(10)),
+        'J' => Ok(Rank(JACK)),
+        'Q' => Ok(Rank(QUEEN)),
+        'K' => Ok(Rank(KING)),
         // some alternate forms are allowed
-        '1' => Rank(ACE),
-        '0' => Rank(10),
-         _  => return Err(err_could_not_parse_card_value!(value_char))
-    };
+        '1' => Ok(Rank(ACE)),
+        '0' => Ok(Rank(10)),
+        _  => Err(err_could_not_parse_card_value!(value_char))
+    }
+}
 
-    let suit = match suit_char {
-        'C' => Suit::Club,
-        'S' => Suit::Spade,
-        'H' => Suit::Heart,
-        'D' => Suit::Diamond,
-        _  => return Err(format!("Could not parse suit: {}", suit_char)),
-    };
-
-    Ok(Card {suit, value,})
+fn parse_suit(suit_char: char) -> Result<Suit, String> {
+    match suit_char {
+        'C' => Ok(Suit::Club),
+        'S' => Ok(Suit::Spade),
+        'H' => Ok(Suit::Heart),
+        'D' => Ok(Suit::Diamond),
+        _  => Err(err_could_not_parse_suit!(suit_char)),
+    }
 }
