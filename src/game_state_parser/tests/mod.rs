9af88c528b8e9f409b88221bfc855f45d9f049d3
Arman Mielke <arman.mielke@yahoo.de>
2018-11-14 20:36:45 +0100

    Add notes for missing tests

--- src/game_state_parser/tests/mod.rs
+++ src/game_state_parser/tests/mod.rs
@@ -1,3 +1,14 @@
 mod test_hard;
 mod test_hard_solved_to_6;
 mod invalid;
+
+/*
+
+missing test cases:
+- something that uses freecells
+    - some freecells
+    - all freecells
+    - too many freecells
+- something that uses some foundations
+
+*/
