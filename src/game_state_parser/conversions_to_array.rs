3f8e2377165054623750441c13198407f610b372
Arman Mielke <arman.mielke@yahoo.de>
2018-11-15 17:45:43 +0100

    Apply band aid fix for files with 7 cascades

--- src/game_state_parser/conversions_to_array.rs
+++ src/game_state_parser/conversions_to_array.rs
@@ -3,7 +3,7 @@ use super::error_messages::{ERR_TOO_MANY_CASCADES, ERR_TOO_MANY_FREECELLS};
 
 
 pub fn cascades_vec_to_array(mut cascades: Vec<Cascade>) -> Result<[Cascade; 8], String> {
-    // TODO rewrite to handle cascades.len() < 8
+    // TODO implement this properly
     if cascades.len() == 8 {
         Ok([
             cascades.remove(0),
@@ -17,6 +17,17 @@ pub fn cascades_vec_to_array(mut cascades: Vec<Cascade>) -> Result<[Cascade; 8],
         ])
     } else if cascades.len() > 8 {
         Err(String::from(ERR_TOO_MANY_CASCADES))
+    } else if cascades.len() == 7 {
+        Ok([
+            cascades.remove(0),
+            cascades.remove(0),
+            cascades.remove(0),
+            cascades.remove(0),
+            cascades.remove(0),
+            cascades.remove(0),
+            cascades.remove(0),
+            Vec::new(),
+        ])
     } else {
         unimplemented!()
     }
