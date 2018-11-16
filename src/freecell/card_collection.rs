08770f8a63f8f6eac23bfed9a8bdfb90023b11d6
Arman Mielke <arman.mielke@yahoo.de>
2018-11-16 16:13:46 +0100

    Add trait CardCollection

---
+++ src/freecell/card_collection.rs
@@ -0,0 +1,19 @@
+use super::Card;
+
+use std::marker::Sized;
+
+
+
+pub trait CardCollection where Self: Sized {
+
+    /// Creates a copy of the CardCollection, but with an additional card added
+    /// Fails if the card cannot be put into the collection.
+    fn add_card(&self, card: Card) -> Result<Self, ()>;
+
+
+    /// Creates all possible versions of the CardCollection, where one card has been removed.
+    /// Each returned CardCollection is paired with the removed card.
+    /// If no card can be removed according to the rules, no CardCollection is returned.
+    fn pop_card(&self) -> Vec<(Self, Card)>;
+
+}
--- src/freecell/mod.rs
+++ src/freecell/mod.rs
@@ -1,10 +1,12 @@
 pub mod card;
 
+mod card_collection;
 mod cascade;
 mod foundation;
 mod game_move;
 mod game_state;
 
+pub use self::card_collection::CardCollection;
 pub use self::card::Card;
 pub use self::cascade::{Cascade, Cascades};
 pub use self::foundation::{Foundation, Foundations, FoundationsTrait};
