3251d01588f4700ab2a14824d7b667bdd5d977fc
Arman Mielke <arman.mielke@yahoo.de>
2018-11-16 17:08:54 +0100

    Implement CardCollection for Freecells

--- src/freecell/freecells.rs
+++ src/freecell/freecells.rs
@@ -1,4 +1,4 @@
-use super::Card;
+use super::{Card, CardCollection};
 
 use arrayvec::ArrayVec;
 
@@ -6,3 +6,31 @@ use arrayvec::ArrayVec;
 
 /// May hold up to four arbitrary cards
 pub type Freecells = ArrayVec<[Card; 4]>;
+
+
+impl CardCollection for Freecells {
+
+    fn add_card(&self, card: Card) -> Result<Self, ()> {
+        if self.is_full() {
+            return Err(())
+        }
+
+        let mut clone = self.clone();
+        clone.push(card);
+        Ok(clone)
+    }
+
+    fn pop_card(&self) -> Vec<(Self, Card)> {
+        let mut results = Vec::new();
+
+        // for each card, return a result where this card has been removed
+        for i in 0..self.len() {
+            let mut clone = self.clone();
+            let card = clone.swap_remove(i);
+            results.push((clone, card))
+        }
+
+        results
+    }
+
+}
