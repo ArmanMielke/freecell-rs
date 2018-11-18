a8918af12ba0805267f819e7a41e22ab1d323d90
Arman Mielke <arman.mielke@yahoo.de>
2018-11-18 18:25:12 +0100

    Implement Display for Position and Move

--- src/freecell/game_move.rs
+++ src/freecell/game_move.rs
@@ -1,9 +1,19 @@
 use super::Card;
 use super::position::Position;
 
+use std::fmt::{Display, Formatter, Result};
+
+
 
 pub struct Move {
     pub card: Card,
     pub from: Position,
     pub to: Position,
+}
+
+
+impl Display for Move {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "Move the {}, from {} to {}", self.card, self.from, self.to)
+    }
 }
--- src/freecell/position.rs
+++ src/freecell/position.rs
@@ -1,6 +1,20 @@
+use std::fmt::{Display, Formatter, Result};
+
+
 
 pub enum Position {
     Cascade(u8),
     Foundations,
     Freecells
+}
+
+
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            Position::Cascade(pos) => write!(f, "Cascade {}", pos),
+            Position::Foundations => write!(f, "the Foundations"),
+            Position::Freecells => write!(f, "the Freecells"),
+        }
+    }
 }
