951783220ff962acd017a8e7b987bd7f779014b7
Arman Mielke <arman.mielke@yahoo.de>
2019-11-14 01:43:18 -0500

    Add test stubs for check_solution()

---
+++ src/tests/check_solution/mod.rs
@@ -0,0 +1,4 @@
+mod check_solution;
+mod tests;
+
+pub use self::check_solution::check_solution;
---
+++ src/tests/check_solution/tests.rs
@@ -0,0 +1,23 @@
+
+//#[test]
+fn test_correct_solution() {
+    // TODO implement
+}
+
+//#[test]
+#[should_panic(expected = "Solution has length TODO, should have length TODO")]
+fn test_wrong_length() {
+    // TODO implement
+}
+
+//#[test]
+#[should_panic(expected = "is not legal at this point in the game.")]
+fn test_illegal_move() {
+    // TODO implement
+}
+
+//#[test]
+#[should_panic(expected = "Following the solution should lead to the solved state, but instead leads to")]
+fn test_does_not_lead_to_solved_state() {
+    // TODO implement
+}
