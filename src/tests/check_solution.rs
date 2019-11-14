3badef3e2b2ad401464017255846ad968c8d9558
Arman Mielke <arman.mielke@yahoo.de>
2019-11-14 01:27:17 -0500

    Implement check_solution_correctness()

--- src/tests/check_solution.rs
+++ src/tests/check_solution.rs
@@ -13,6 +13,32 @@ fn check_solution_length(solution: &Vec<Move>, correct_length: usize) {
     }
 }
 
+/// Checks whether the solution is a valid sequence of moves that leads to the solved state.
 fn check_solution_correctness(solution: Vec<Move>, initial_state: GameState) {
-    // TODO implement
+    let mut current_state = initial_state;
+
+    // check whether each of the moves is legal at that point in the game
+    for (move_number, solution_move) in solution.iter().enumerate() {
+        let mut next_state = None;
+
+        for (resulting_state, legal_move) in current_state.get_legal_moves() {
+            if solution_move.eq(&legal_move) {
+                next_state = Some(resulting_state);
+                break;
+            }
+        }
+
+        // if next_state is None that means the solution's move did not match any of the legal moves
+        current_state = match next_state {
+            Some(game_state) => game_state,
+            None => panic!(
+                "Move {} is not legal at this point in the game.\nMove: {}\nGameState: {:#?}",
+                move_number + 1, solution_move, current_state
+            )
+        }
+    }
+
+    if !current_state.is_solved() {
+        panic!("Following the solution should lead to the solved state, but instead leads to {:#?}", current_state)
+    }
 }
