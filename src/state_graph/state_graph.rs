09a40b8a11afce31677f1202ff6b869d49cf497c
Arman Mielke <arman.mielke@yahoo.de>
2019-11-18 04:20:28 -0500

    Fix Warning
    Warning "value assigned to `node_has_been_added_before` is never read"
    at src/state_graph/state_graph.rs:39:17

--- src/state_graph/state_graph.rs
+++ src/state_graph/state_graph.rs
@@ -36,8 +36,7 @@ impl StateGraph {
 
     pub(super) fn add_node(&mut self, game_state: GameState) {
         // only add the node if it has not been added before
-        let mut node_has_been_added_before = false;
-        { node_has_been_added_before = self.nodes.contains_key(&game_state.id()) }
+        let node_has_been_added_before = { self.nodes.contains_key(&game_state.id()) };
         if !node_has_been_added_before {
             let node = Node::new(game_state);
             self.nodes.insert(node.id, node);
