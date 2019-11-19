837beff58a6fb9001e8d240cad243e370361d568
Arman Mielke <arman.mielke@yahoo.de>
2019-11-18 21:01:40 -0500

    Add fn rank_from_string()

--- src/freecell/card/rank.rs
+++ src/freecell/card/rank.rs
@@ -1,8 +1,9 @@
 
+pub const ACE: Rank = 1;
 pub const JACK: Rank = 11;
 pub const QUEEN: Rank = 12;
 pub const KING: Rank = 13;
-pub const ACE: Rank = 1;
+
 
 /// Indicates the rank of a card.
 ///
@@ -15,3 +16,29 @@ pub const ACE: Rank = 1;
 ///
 /// Rank 0 and ranks greater than 13 are not valid card ranks.
 pub type Rank = u8;
+
+
+pub fn rank_from_string(string: String) -> Result<Rank, &'static str> {
+    match string.trim().to_lowercase().as_ref() {
+        "ace" => Ok(ACE),
+        "a" => Ok(ACE),
+        "jack" => Ok(JACK),
+        "j" => Ok(JACK),
+        "queen" => Ok(QUEEN),
+        "q" => Ok(QUEEN),
+        "king" => Ok(KING),
+        "k" => Ok(KING),
+
+        // rank is not a word => try to parse it as u8
+        _ => match string.trim().parse::<u8>() {
+            Err(_) => return Err("Could not parse integer"),
+
+            // successfully parsed number => check whether it is in the correct range
+            Ok(rank) => match rank {
+                0 => Err("Rank cannot be 0"),
+                1...13 => Ok(rank),
+                _ => Err("Rank cannot be greater than 13"),
+            }
+        }
+    }
+}
