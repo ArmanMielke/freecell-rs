6aa34e693794a1227e4169f91f3555c74f86b6de
Arman Mielke <arman.mielke@yahoo.de>
2019-11-25 22:38:38 -0500

    Improve module structure
    Modules for card collections and modules for moves were grouped
    into the modules card_collections and game_move respectively.

---
+++ src/game_move/mod.rs
@@ -0,0 +1,5 @@
+mod game_move;
+mod position;
+
+pub use self::game_move::Move;
+pub use self::position::Position;
--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,11 @@
 pub mod game_state_parser;
 
 mod card;
-mod card_collection;
-mod cascade;
-mod foundation;
-mod freecells;
+mod card_collections;
 mod game_move;
 mod game_state;
-mod position;
 
 pub use self::card::{rank_from_string, Card, Color, Colour, Rank, Suit, ACE, JACK, KING, QUEEN};
-pub use self::card_collection::CardCollection;
-pub use self::cascade::{Cascade, Cascades};
-pub use self::foundation::{Foundation, Foundations};
-pub use self::freecells::Freecells;
-pub use self::game_move::Move;
+pub use self::card_collections::{CardCollection, Cascade, Cascades, Foundation, Foundations, Freecells};
+pub use self::game_move::{Move, Position};
 pub use self::game_state::{GameState, GameStateId};
-pub use self::position::Position;
