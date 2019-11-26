bb2236f8176ebbbc3118b35292af4d1d0185b1e6
Arman Mielke <arman.mielke@yahoo.de>
2019-11-26 15:19:33 -0500

    Add tests for Cascade.pop_card()

--- tests/card_collections_tests/cascade/pop_card.rs
+++ tests/card_collections_tests/cascade/pop_card.rs
@@ -1,1 +1,26 @@
-// TODO test
+use freecell::game_state_parser;
+use freecell::Suit::{Diamond, Heart, Spade};
+use freecell::{Card, CardCollection, Cascade};
+
+#[test]
+fn test_empty() {
+    let cascade: Cascade = Vec::new();
+    assert_eq!(cascade.pop_card(), Vec::new());
+}
+
+#[test]
+fn test_not_empty() {
+    let game_state = game_state_parser::parse_file("test-inputs/hard-solved-to-5.txt").unwrap();
+    let expected = vec![
+        (
+            vec![
+                Card { suit: Diamond, rank: 10 },
+                Card { suit: Heart, rank: 9 },
+                Card { suit: Spade, rank: 9 },
+                Card { suit: Diamond, rank: 6 },
+            ],
+            Card { suit: Spade, rank: 6 }
+        )
+    ];
+    assert_eq!(game_state.cascades[3].pop_card(), expected);
+}
