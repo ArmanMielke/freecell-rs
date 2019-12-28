02cb0ca8056690f74bbe2e284ce9e57d4ae55cfb
Arman Mielke <arman.mielke@yahoo.de>
2019-12-28 12:11:56 -0500

    Refactor const test card array

--- tests/card_tests/card_format_consistent.rs
+++ tests/card_tests/card_format_consistent.rs
@@ -1,7 +1,7 @@
 use freecell::Suit::{Club, Diamond, Heart, Spade};
 use freecell::{Card, ACE, JACK, KING, QUEEN};
 
-const CARDS: &'static [Card] = &[
+const CARDS: [Card; 6] = [
     Card { suit: Heart, rank: ACE},
     Card { suit: Diamond, rank: 5},
     Card { suit: Spade, rank: 10},
@@ -12,14 +12,14 @@ const CARDS: &'static [Card] = &[
 
 #[test]
 fn test_display() {
-    for card in CARDS {
-        assert_eq!(Ok(*card), card.to_string().parse());
+    for &card in &CARDS {
+        assert_eq!(Ok(card), card.to_string().parse());
     }
 }
 
 #[test]
 fn test_debug() {
-    for card in CARDS {
-        assert_eq!(Ok(*card), format!("{:?}", card).parse());
+    for &card in &CARDS {
+        assert_eq!(Ok(card), format!("{:?}", card).parse());
     }
 }
